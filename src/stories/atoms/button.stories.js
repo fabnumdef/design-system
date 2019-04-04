d8e66a83b884bd298f5e04c8ab3d4c3221b6e04c
Gaël <gael+git@demette.fr>
2019-04-04 15:14:00 +0200

    Make stories exportables

--- src/stories/atoms/button.stories.js
+++ src/stories/atoms/button.stories.js
@@ -5,54 +5,60 @@ import {
   withKnobs, select, text, boolean,
 } from '@storybook/addon-knobs';
 
-import fnmaButton, { FULLWIDTH, OUTLINED, INVERTED } from '../../components/atoms/button.vue';
+import cButton, { FULLWIDTH, OUTLINED, INVERTED } from '../../components/atoms/button.vue';
 import {
   COLORS, GRAYSCALE, STATES, TEXT,
 } from '../../mixins/generic-modifiers';
 
-storiesOf('Atoms/Button', module)
-  .addDecorator(withKnobs)
-  .add('with options', () => {
-    const options = ['', ...COLORS, ...GRAYSCALE, TEXT];
-    const moreSwitchs = [FULLWIDTH, OUTLINED, INVERTED, ...STATES];
-    return {
-      components: { fnmaButton },
-      template: `
+export function add(stories, fnmaButton = cButton) {
+  stories
+    .addDecorator(withKnobs)
+    .add('with options', () => {
+      const options = ['', ...COLORS, ...GRAYSCALE, TEXT];
+      const moreSwitchs = [FULLWIDTH, OUTLINED, INVERTED, ...STATES];
+      return {
+        components: { fnmaButton },
+        template: `
       <fnma-button v-bind="globalStyle" @click="action">
         {{text}}
       </fnma-button>`,
-      methods: { action: action('clicked !') },
-      props: {
-        text: {
-          type: String,
-          default: text('Text', 'Lorem Ipsum'),
+        methods: { action: action('clicked !') },
+        props: {
+          text: {
+            type: String,
+            default: text('Text', 'Lorem Ipsum'),
+          },
+          styleSelect: {
+            type: String,
+            default: select('Style', options, null),
+          },
+          ...moreSwitchs
+            .map(r => ({
+              [r]: {
+                type: Boolean,
+                default: boolean(r, false),
+              },
+            }))
+            .reduce((acc, cur) => Object.assign(acc, cur), {}),
         },
-        styleSelect: {
-          type: String,
-          default: select('Style', options, null),
-        },
-        ...moreSwitchs
-          .map(r => ({
-            [r]: {
-              type: Boolean,
-              default: boolean(r, false),
-            },
-          }))
-          .reduce((acc, cur) => Object.assign(acc, cur), {}),
-      },
-      computed: {
-        globalStyle() {
-          return {
-            ...[
-              ...options
-                .filter(o => o !== '')
-                .map(o => ({ [o]: o === this.styleSelect })),
-              ...moreSwitchs.map(s => ({ [s]: !!this[s] })),
-            ]
-              .reduce((acc, cur) => Object.assign(acc, cur), {}),
+        computed: {
+          globalStyle() {
+            return {
+              ...[
+                ...options
+                  .filter(o => o !== '')
+                  .map(o => ({ [o]: o === this.styleSelect })),
+                ...moreSwitchs.map(s => ({ [s]: !!this[s] })),
+              ]
+                .reduce((acc, cur) => Object.assign(acc, cur), {}),
 
-          };
+            };
+          },
         },
-      },
-    };
-  });
+      };
+    });
+}
+
+export default () => {
+  add(storiesOf('Atoms/Button', module));
+};
